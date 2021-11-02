b6cdad041e9e935b01ad3a7252aed534bf6586ba
COSMO ANDRADE FELIX <57240412+CosmoAndrade@users.noreply.github.com>
2021-11-02 09:53:06 -0300

    Projeto filmaria com React

---
+++ Filmaria-ReactJs/src/pages/Filme/index.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+
+import './filme-info.css'
+
+import { useParams , useHistory } from "react-router";
+
+import api from '../../services/api'
+
+
+function Filme(){
+
+    const {id} = useParams();
+    const history = useHistory ()
+
+    const [filme, setFilme] = useState([])
+    const [loading , setLoading] = useState(true)
+
+    useEffect(() => {
+        async function loadFilme(){
+    const response = await api.get(`r-api/?api=filmes/${id}`)
+
+    if (response.data.lenght === 0){
+        history.replace('/');
+        return;
+    }
+
+    setFilme(response.data)
+    setLoading(false)
+
+        }
+        loadFilme()
+
+        return () => {
+
+        }
+
+    },[history,id])
+
+    function salvaFilme(){
+        const minhaLista = localStorage.getItem('filmes')
+
+        let filmesSalvos = JSON.parse(minhaLista) || [];
+
+        const hasFilme = filmesSalvos.some((filmesSalvo) => filmesSalvo.id === filme.id )
+
+        if (hasFilme){
+            return;
+        }
+
+        filmesSalvos.push(filme);
+        localStorage.setItem('filmes',JSON.stringify(filmesSalvos))
+
+
+    }
+
+
+    if (loading){
+        return(
+            <div className="filme-info">
+            <h1>Carregando seu filme</h1>
+            </div>
+        )
+    }
+
+    return(
+        <div className="filme-info">
+            <h1>{filme.nome}</h1>
+            <img src={filme.foto} alt ={filme.nome}/>
+
+            <h3>Sinopse</h3>
+
+            {filme.sinopse}
+
+            <div className="botoes">
+                <button onClick={salvaFilme} >Salvar</button>
+                <button>
+<a target="_blank" href={`https://youtube.com/results?search_query=${filme.nome} Trailer`}>
+                        Trailer
+                    </a>
+                </button>
+            </div>
+
+
+        </div>
+    )
+}
+
+
+export default Filme;
